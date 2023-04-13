096c8c37a83f32d867db7cc4fa35a25eaf166cd4
ruanpablo <ruanpablo.santosdejesus1@gmail.com>
2023-04-13 01:02:43 -0300

    Inicializando meu sistema de cadastro

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Olá, bem-vindo ao nosso sistema de cadastro de usuário!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
