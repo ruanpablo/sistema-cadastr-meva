b5adfba08eb31c1162ab3208cf89904e9842cc3c
ruanpablo <ruanpablo.santosdejesus1@gmail.com>
2023-04-13 01:42:37 -0300

    Criando classe usuário

---
+++ src/Usuario.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Usuario {
+    private String nome;
+    private int cpf;
+    private LocalDate dataDeNascimento;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
